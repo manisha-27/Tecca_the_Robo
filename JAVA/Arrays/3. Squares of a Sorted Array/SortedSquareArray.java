855cf4fd7af61c96a54ca4f7e03fadda648a67ec
manisha-27 <67019423+manisha-27@users.noreply.github.com>
2021-12-13 11:17:31 +0530

    Update SortedSquareArray.java

--- JAVA/Arrays/3. Squares of a Sorted Array/SortedSquareArray.java
+++ JAVA/Arrays/3. Squares of a Sorted Array/SortedSquareArray.java
@@ -6,6 +6,17 @@ class Solution {
             nums[i]=nums[i]*nums[i];
         }
         Arrays.sort(nums);
+//         OR
+//         for(int i=0;i<nums.length;i++){
+//                 for(int j=0;j<nums.length;j++){
+//                     if(nums[j]>nums[i]){
+//                      int temp;
+//                 temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;   
+//                     }
+//               }
+//         }
         return nums;
     }
 }
