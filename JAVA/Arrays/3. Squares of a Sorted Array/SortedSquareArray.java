0439de29d184f404206ed526d6064aee8bcccf7e
manisha-27 <67019423+manisha-27@users.noreply.github.com>
2021-12-13 11:11:39 +0530

    Create SortedSquareArray.java

---
+++ JAVA/Arrays/3. Squares of a Sorted Array/SortedSquareArray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            // nums[i]=Math.pow(nums[i], 2);
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
