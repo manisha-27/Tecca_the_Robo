059ff8465ae044590f4bca44baa150b55a70b62a
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2022-08-03 19:44:44 +0530

    Brute Force Approch Update

--- LeetCode/Running Sum of 1d Array/Solution1.java
+++ LeetCode/Running Sum of 1d Array/Solution1.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i=1;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             if(i==0)
                 nums[0]=nums[0];
             else{
