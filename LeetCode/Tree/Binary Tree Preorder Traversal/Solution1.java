12d70ebb8ef8693d3ae7c1c801b98ad3a4e2e597
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2022-10-04 23:43:27 +0530

    Add Complete Program

--- LeetCode/Tree/Binary Tree Preorder Traversal/Solution1.java
+++ LeetCode/Tree/Binary Tree Preorder Traversal/Solution1.java
@@ -1,19 +1,20 @@
-/** //Added Binary Tree Node Defination for Reference
- * Definition for a binary tree node.
- * public class TreeNode{
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+import java.util.*;
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class PreorderTraversal {
+     TreeNode root;
     public List<Integer> preorderTraversal(TreeNode root) {
         Stack<TreeNode> st=new Stack<TreeNode>();
         List<Integer> li=new ArrayList<Integer>();
@@ -30,4 +31,56 @@ class Solution {
         }
         return li;
     }
+
+    public static void main(String[] args) {
+        PreorderTraversal pt=new PreorderTraversal();
+        pt.root = new TreeNode(37);
+        pt.root.left = new TreeNode(27);
+        pt.root.right = new TreeNode(47);
+        pt.root.left.left = new TreeNode(22);
+        pt.root.left.right = new TreeNode(32);
+        pt.root.left.left.left = new TreeNode(12);
+        pt.root.left.left.right = new TreeNode(25);
+        pt.root.right.left = new TreeNode(42);
+        pt.root.right.right = new TreeNode(57);
+        pt.root.right.right.left = new TreeNode(52);
+        pt.root.right.right.right = new TreeNode(67);
+
+        System.out.println(pt.preorderTraversal(pt.root));
+    }
 }
+
+// /** //Added Binary Tree Node Defination for Reference
+//  * Definition for a binary tree node.
+//  * public class TreeNode{
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         Stack<TreeNode> st=new Stack<TreeNode>();
+//         List<Integer> li=new ArrayList<Integer>();
+//         if(root==null)
+//             return li;
+//         st.push(root);
+//         while(!st.isEmpty()){
+//             root=st.pop();
+//             if(root.right!=null)
+//                 st.push(root.right);
+//             if(root.left!=null)
+//                 st.push(root.left);
+//             li.add(root.val);
+//         }
+//         return li;
+//     }
+// }
+
