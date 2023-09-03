fbb54e0fdd964d7f2c69b008a14f6338f8803d03
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-09-03 14:58:56 +0530

    Time: 0 ms (100.00%), Space: 7 MB (47.50%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-
+    int a=0;
+    map<string,string> tinyURL;
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        string s="https://tinyurl.com/"+to_string(a);
+        a++;
+        tinyURL[s]=longUrl;
+        return s;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return tinyURL[shortUrl];
     }
 };
 
