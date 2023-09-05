147eca2361345358d4ce7f1bbbd79f4f2c4124fc
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-09-05 15:51:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1)
+            return n;
+        int one=1;  // 2nd last
+        int two=1;  // last
+        int sum=one+two;
+        n=n-2;
+        while(n>0){
+            two=one;
+            one=sum;
+            sum=one+two;
+            n--;
+        }
+        return sum;
+    }
+};
