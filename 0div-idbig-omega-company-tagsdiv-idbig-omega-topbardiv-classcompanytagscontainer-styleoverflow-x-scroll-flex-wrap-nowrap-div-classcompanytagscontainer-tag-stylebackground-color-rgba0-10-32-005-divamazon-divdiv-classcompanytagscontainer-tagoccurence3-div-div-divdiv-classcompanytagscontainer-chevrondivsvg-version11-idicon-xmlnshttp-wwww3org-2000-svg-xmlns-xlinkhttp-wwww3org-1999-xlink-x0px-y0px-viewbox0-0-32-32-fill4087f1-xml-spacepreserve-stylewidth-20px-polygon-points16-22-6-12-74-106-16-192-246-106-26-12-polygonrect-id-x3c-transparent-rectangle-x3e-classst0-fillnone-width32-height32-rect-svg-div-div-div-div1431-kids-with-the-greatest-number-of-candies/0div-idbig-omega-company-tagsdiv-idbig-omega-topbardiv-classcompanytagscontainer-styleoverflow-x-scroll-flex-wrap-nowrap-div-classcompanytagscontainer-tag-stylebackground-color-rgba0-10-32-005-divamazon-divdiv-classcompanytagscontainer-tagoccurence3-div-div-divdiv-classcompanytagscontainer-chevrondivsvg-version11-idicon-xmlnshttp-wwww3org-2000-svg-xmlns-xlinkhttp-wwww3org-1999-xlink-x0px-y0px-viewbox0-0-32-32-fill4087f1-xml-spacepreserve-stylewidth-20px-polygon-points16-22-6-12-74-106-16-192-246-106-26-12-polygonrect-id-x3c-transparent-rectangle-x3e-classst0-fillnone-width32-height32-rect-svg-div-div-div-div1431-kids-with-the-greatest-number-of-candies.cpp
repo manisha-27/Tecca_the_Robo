a99294f6b6f468abf048c5c4d972431c2195da27
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-11-02 23:59:28 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (74.41%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1431-kids-with-the-greatest-number-of-candies/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi=0;
+        for(int i=0;i<candies.size();i++){
+            maxi=max(maxi,candies[i]);
+        }
+        vector<bool> great;
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]+extraCandies>=maxi)
+                great.push_back(true);
+            else
+                great.push_back(false);
+        }
+        return great;
+        // int maxi=0;
+        // for(int i=0;i<candies.size();i++){
+        //     maxi=max(maxi,candies[i]);
+        // }
+        // vector<bool> great;
+        // for(int i=0;i<candies.size();i++){
+        //     int a=candies[i]+extraCandies;
+        //     if(a>=maxi)
+        //         great.push_back(true);
+        //     else
+        //         great.push_back(false);
+        // }
+        // return great;
+    }
+};
