d1eb051543d073bec26a542c9650196dbb9e7cb9
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-11-30 21:49:20 +0530

    Time: 8 ms (68.04%), Space: 22.1 MB (67.66%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -15,17 +15,14 @@ public:
         queue<TreeNode*> qe;
         qe.push(root);
         int a=qe.front()->val;
-        // vector<int> ele;
         while(!qe.empty()){
             int s=qe.size();
-            // ele.clear();
             a=qe.front()->val;
             while(s>0){
                 TreeNode* temp=qe.front();
                 qe.pop();
                 if(temp->left!=NULL) qe.push(temp->left);
                 if(temp->right!=NULL) qe.push(temp->right);
-                // ele.push_back(temp->val);
                 s--;
             }
         }
