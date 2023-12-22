64e2e952e919b370ed0c26b682c255959ded127e
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-12-22 20:05:23 +0530

    Time: 8 ms (44.76%), Space: 19.4 MB (9.03%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,9 +15,9 @@ public:
         if(root==NULL)
             return 0;
         int left=0,right=0;
-        if(root->left!=NULL)
+        // if(root->left!=NULL)
             left=maxDepth(root->left);
-        if(root->right!=NULL)
+        // if(root->right!=NULL)
             right=maxDepth(root->right);
         return max(left,right)+1;
     }
