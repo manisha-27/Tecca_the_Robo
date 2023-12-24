6ed006086c0d4e8361b3586277ac624cf14188bf
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-12-24 13:39:54 +0530

    Time: 7 ms (8.31%), Space: 7.3 MB (45.10%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,33 +1,12 @@
 class Solution {
 public:
-    int checkString(string s,char c){
-        // int l=s.size();
-        int i=0,count=0;
-        // string st="";
-        char d;
-        if(c=='1')
-            d='0';
-        else
-            d='1';
-        while(i<s.size()){
-            char m;
-            if(i%2==0){
-                // st+=c;
-                m=c;
-            }else{
-                // st+=d;
-                m=d;
-            }
-            if(m!=s[i])
-                count++;
-            i++;
-        }
-        return count;
-    }
     int minOperations(string s) {
-        int mini=INT_MAX;
-        mini=min(mini,checkString(s,'1'));
-        mini=min(mini,checkString(s,'0'));
-        return mini;
+        int mini=0;
+        for(int i=0;i<s.size();i++){
+            char ep = (i % 2 == 0) ? '0' : '1';
+            if(ep!=s[i])
+                mini++;
+        }
+        return min(mini,(int)(s.size()-mini));
     }
 };
