a44cc733e3cf21fe4a84818f0eed49f63aa21e01
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2023-12-27 22:21:02 +0530

    Time: 1627 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
-# select e1.name
-# from employee e1 left join employee e2 on e1.id=e2.managerId
-# group by e1.id
-# having count(e2.name)>=5 or count(e2.name) is null and e1.id is not null
+select e1.name
+from employee e1 left join employee e2 on e1.id=e2.managerId
+group by e1.id
+having count(e1.id)>=5
 
-Select m.name
-from employee as e
-inner join employee as m
-on e.managerId=m.id
-group by e.managerId 
-having count(e.id)>=5
+# Select m.name
+# from employee as e
+# inner join employee as m
+# on e.managerId=m.id
+# group by e.managerId 
+# having count(e.id)>=5
 
 # SELECT e1.name
 # FROM employee e1
