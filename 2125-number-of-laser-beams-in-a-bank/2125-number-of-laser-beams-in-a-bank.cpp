0caa0bf8230108a95b878ce3260823879ba4e8ed
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2024-01-04 02:29:54 +0530

    Time: 93 ms (49.18%), Space: 23.3 MB (96.73%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-    int countBits(string n)
-    {
-        int setBits=0;
-        for (char c : n) {
-            if (c == '1') {
-                setBits++;
-            }
-        }
-        return setBits;
-    }
     int numberOfBeams(vector<string>& bank) {
-        int i=0,j=0;
-        int count=0,t=0;
-        map<int,int> security;
-        while(t<bank.size()){
-            security[t]=countBits(bank[t]);
-            t++;
-        }
-        // security[0]=3;
-        // security[1]=0;
-        // security[2]=2;
-        // security[3]=1;
-        for(auto a: security){
-            if(a.second!=0){
-                j=a.second;
-            count+=(j*i);
-            i=j;}
+         int count = 0, i = 0, j = 0;
+        for (const auto& row : bank) {
+            int setBits = std::count(row.begin(), row.end(), '1');
+            if (setBits != 0) {
+                j=setBits;
+                count += (j * i);
+                i = setBits;
+            }
         }
         return count;
     }
