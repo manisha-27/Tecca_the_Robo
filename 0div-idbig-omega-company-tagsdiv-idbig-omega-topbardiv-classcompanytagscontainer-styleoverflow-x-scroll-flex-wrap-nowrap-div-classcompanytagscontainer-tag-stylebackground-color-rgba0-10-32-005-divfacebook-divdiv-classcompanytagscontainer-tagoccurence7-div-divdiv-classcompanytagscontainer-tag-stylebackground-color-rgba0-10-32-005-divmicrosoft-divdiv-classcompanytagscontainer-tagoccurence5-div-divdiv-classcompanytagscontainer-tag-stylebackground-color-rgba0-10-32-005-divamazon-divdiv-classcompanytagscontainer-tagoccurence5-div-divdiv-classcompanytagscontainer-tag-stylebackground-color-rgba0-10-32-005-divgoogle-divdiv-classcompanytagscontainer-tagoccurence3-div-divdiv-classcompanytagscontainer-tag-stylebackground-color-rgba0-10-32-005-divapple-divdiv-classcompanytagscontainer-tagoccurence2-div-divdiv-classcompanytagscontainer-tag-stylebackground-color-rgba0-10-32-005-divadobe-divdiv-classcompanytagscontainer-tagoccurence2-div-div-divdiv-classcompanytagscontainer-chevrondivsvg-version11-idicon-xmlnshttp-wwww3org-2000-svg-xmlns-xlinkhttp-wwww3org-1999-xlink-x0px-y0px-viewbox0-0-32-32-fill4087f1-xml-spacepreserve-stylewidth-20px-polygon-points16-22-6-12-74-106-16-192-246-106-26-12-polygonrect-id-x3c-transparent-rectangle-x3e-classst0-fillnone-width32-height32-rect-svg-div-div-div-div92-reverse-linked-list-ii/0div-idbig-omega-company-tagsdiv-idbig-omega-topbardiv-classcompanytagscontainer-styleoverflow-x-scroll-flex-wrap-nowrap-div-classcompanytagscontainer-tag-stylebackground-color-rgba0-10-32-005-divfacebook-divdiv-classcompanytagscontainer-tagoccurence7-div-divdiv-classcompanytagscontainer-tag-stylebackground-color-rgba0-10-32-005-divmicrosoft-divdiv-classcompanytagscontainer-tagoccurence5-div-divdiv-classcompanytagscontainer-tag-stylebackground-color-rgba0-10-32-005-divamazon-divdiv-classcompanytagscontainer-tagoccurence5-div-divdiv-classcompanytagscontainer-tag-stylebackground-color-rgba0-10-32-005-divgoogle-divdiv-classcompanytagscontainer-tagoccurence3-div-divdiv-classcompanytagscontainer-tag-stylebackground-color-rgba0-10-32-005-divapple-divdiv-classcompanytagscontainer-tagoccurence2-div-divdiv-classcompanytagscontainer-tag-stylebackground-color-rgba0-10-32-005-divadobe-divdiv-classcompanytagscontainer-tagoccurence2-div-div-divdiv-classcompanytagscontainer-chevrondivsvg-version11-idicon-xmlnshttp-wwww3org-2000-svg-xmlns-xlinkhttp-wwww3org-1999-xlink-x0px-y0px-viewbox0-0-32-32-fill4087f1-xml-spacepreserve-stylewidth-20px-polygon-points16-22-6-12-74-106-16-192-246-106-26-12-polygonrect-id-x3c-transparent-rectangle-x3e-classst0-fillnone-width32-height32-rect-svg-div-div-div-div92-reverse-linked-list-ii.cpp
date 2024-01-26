1ef44e1a6437d6b864c2830974f7a39ca40dce43
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2024-01-27 00:47:18 +0530

    Time: 4 ms (41.49%), Space: 9.3 MB (6.34%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div92-reverse-linked-list-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div92-reverse-linked-list-ii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div92-reverse-linked-list-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div92-reverse-linked-list-ii.cpp
@@ -11,31 +11,27 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (head == nullptr || head->next == nullptr || left >= right)
+        if(head==NULL || head->next==NULL || left>=right)
             return head;
-
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* prev1 = dummy;
-        ListNode* curr1 = head;
-
-        // Move to the starting position
-        for (int i = 1; i < left; i++) {
-            prev1 = curr1;
-            curr1 = curr1->next;
+        ListNode* dummy=new ListNode();
+        dummy->next=head;
+        ListNode* prev1=dummy;
+        ListNode* curr1=head;
+        for(int i=1;i<left;i++){
+            prev1=curr1;
+            curr1=curr1->next;
         }
-
-        ListNode* prev2 = prev1;
-        ListNode* curr2 = curr1;
-
-        // Move to the ending position
-        for (int i = left; i <= right; i++) {
-            prev2 = curr2;
-            curr2 = curr2->next;
+        
+        ListNode* prev2=prev1;
+        ListNode* curr2=head;
+        for(int i=1;i<right;i++){
+            // prev1=curr1;
+            curr2=curr2->next;
         }
-
-        prev2->next = nullptr;  // disconnect the sublist
-
+        prev1->next=NULL;
+        prev2=curr2->next;
+        curr2->next=NULL;
+        // curr2=curr1;
         // Reverse the sublist
         ListNode* prev = nullptr;
         ListNode* temp = curr1;
@@ -45,52 +41,9 @@ public:
             prev = temp;
             temp = next;
         }
-
-        // Connect the reversed sublist back to the main list
-        prev1->next = prev;
-        curr1->next = curr2;
-
-        return dummy->next;  // return the updated head
+        prev1->next=prev;
+        curr1->next=prev2;
+        
+        return dummy->next;
     }
 };
-
-// class Solution {
-// public:
-//     ListNode* reverseBetween(ListNode* head, int left, int right) {
-//         if(head==NULL || head->next==NULL || left>=right)
-//             return head;
-//         ListNode* curr1=head;
-//         // ListNode* temp=curr->next;
-//         ListNode* prev1=nullptr;
-//         ListNode* curr2=head;
-//         ListNode* prev2=nullptr;
-//         while(left-->1){
-//             // cout<<left<<" ";
-//             prev1=curr1;
-//             curr1=curr1->next;
-//         }
-//         // cout<<endl;
-//         while(right-->1){
-//             // cout<<right<<" ";
-//             // prev2=curr2;
-//             curr2=curr2->next;
-//         }
-//         prev2=curr2->next;  // save 5
-//         curr2->next=NULL;
-//         curr2=curr1;
-//         {
-//             ListNode* prev=nullptr;
-//             ListNode* temp=curr1;
-//             while(temp!=NULL){
-//                 temp=temp->next;
-//                 curr1->next=prev;
-//                 // temp->next=curr1;
-//                 prev=curr1;
-//                 curr1=temp;
-//             }
-//             prev1->next=prev;
-//         }
-//         curr2->next=prev2;
-//         return head;
-//     }
-// };
